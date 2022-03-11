1fdc829e168d7183320179e87677bfcc3e1a96b1
Elena <krollik2014@mail.ru>
2022-03-11 17:07:25 +0500

    Add comment

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
-
+#include<windows.h>
 using namespace std;
 
 int main()
 {
+    SetConsoleOutputCP(1251);
     int a,b,c;
  cout<<"Add 2 numbers\n";
+ cout<<"a=";
  cin>>a;
+ cout<<"b=";
  cin>>b;
  c=a+b;
  cout<<c;

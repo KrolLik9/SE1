0a68cb8ecf742f5db1a4f6c52a2d74f01a865ccd
Elena <krollik2014@mail.ru>
2022-03-11 18:48:58 +0500

    add print diff

--- main.cpp
+++ main.cpp
@@ -5,13 +5,17 @@ using namespace std;
 int main()
 {
     SetConsoleOutputCP(1251);
-    int a,b,c;
+    int a,b,c,d;
  cout<<"Add 2 numbers\n";
  cout<<"a=";
  cin>>a;
  cout<<"b=";
  cin>>b;
  c=a+b;
+ d=a-b;
+ cout<<"Diff = ";
+ cout<<d;
+ cout<<"\n Summ = ";
  cout<<c;
     return 0;
 }
